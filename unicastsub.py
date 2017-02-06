4d9733b6be4203848892942324d1fc33f7ab7149
pengminjiong <pengminjiong19911004@outlook.com>
2017-02-06 15:52:49 +0000

    thread

--- unicastsub.py
+++ unicastsub.py
@@ -6,7 +6,7 @@ context = zmq.Context()
 #  Socket to talk to server
 print("Connecting to hello world server")
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://172.17.0.8:5555")
+socket.connect("tcp://172.17.0.2:5555")
 socket.setsockopt(zmq.SUBSCRIBE, "")
 #  Do 10 requests, waiting each time for a response
 #for request in range(10):
